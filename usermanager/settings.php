12caf2e4f6fd4eb2752a223190fd524a85aae426
iPaddy <prvink@prv.nl>
2013-07-06 15:42:36 +0200

    Added Logo, Copyright and Background Color as a setting

--- usermanager/settings.php
+++ usermanager/settings.php
@@ -3,9 +3,15 @@
 
 //General Settings
 $appName="YourAppName"; //Your App Name
-$emailFromName="AppName Support"; //Email from name
-$emailFrom="support@appname.com"; //Email from address
-$emailCC="support@appname.com"; //Email bcc address
+$appLogo=""; //Your App logo (optional value)
+$appLogoName=""; //Your App name For the App logo (optional value)
+$copyright="&copy; YourCompany 2013"; //Your copyright text
+$backgroundColor="#f5f5f5"; //Web site background color
+
+//Email Settings
+$emailFromName="AppName Support"; //Email From name
+$emailFrom="support@appname.com"; //Email From address
+$emailBCC="support@appname.com"; //Email BCC address (optional value)
     
 //Simperium Settings
 $app_id="YourAppID"; //Simperium App ID, found in Simperium dashboard
